4fae907b58e6b5fe346832f9af8d7df740cc1aa2
khadija alhyane <khadijaalhyane321@gmail.com>
2026-02-22 23:10:18 +0000

    Add text cleaning and tokenization functions
    Added stop_words set and implemented text cleaning and tokenization functions.

--- utils.py
+++ utils.py
@@ -1,1 +1,24 @@
+stop_words={"a","an","the","in","at","on","or","and","with","is","are","they",
+            "but","you","it","we","before","after","he","she","to","from","was",
+            "were","had","have","each","every","be","being","throught","within","of","into"}
 
+def clean_text(text) :
+    text=text.lower()
+    clean=""
+    
+    for char in text :
+        if char.isalpha() or char==" " :
+            clean += char
+            
+    return clean
+    
+    
+def tokenize(text) :
+    words=text.split()
+    tokens = []
+    
+    for word in words :
+        if word not in stop_words :
+            tokens.append(word)
+            
+    return tokens
