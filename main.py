1bd0175aeb82046eeb1dbb1e2337f9bca7c67ef2
khadija alhyane <khadijaalhyane321@gmail.com>
2026-02-23 22:34:13 +0000

    Implement basic search functionality with user input

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from search import search_query
+from indexer import indexer
 
+index=indexer()
+while True:
+    user_query = input("Enter query (or 'exit' to quit): ")
+    if user_query.lower() == 'exit':
+        break
+    results = search_query(user_query, index)
+    for doc, score in results[:5]:
+        print(doc, score)
