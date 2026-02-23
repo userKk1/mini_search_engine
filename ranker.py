7a66efd5b14f96940361aa2f8f4c365308bb0e92
khadija alhyane <khadijaalhyane321@gmail.com>
2026-02-23 07:07:12 +0000

    Refactor scoring mechanism in ranker.py

--- ranker.py
+++ ranker.py
@@ -7,19 +7,20 @@ def ranker(query, index) :
     text=clean_text(query)
     words=tokenize(text)
     query_words=Counter(words)
-    scores=defaultdict(lambda : {"match":0,"freq":0})
-    results={}
+    scores=defaultdict(float)
+    IDF={}
+    
+    total_docs = len({doc for word in index for doc in index[word]})
     
     for word, w_count in query_words.items() :
         if word not in index:
             continue
-    
-        for doc in index[word] :
-            scores[doc]["freq"]+=index[word][doc]*w_count
-            scores[doc]["match"]+=1
-            
-    for doc in scores:
-        results[doc]=scores[doc]["match"]*1000+scores[doc]["freq"]
         
-    results=sorted(results.items(), key=lambda x:x[1], reverse=True)
-    return results
+        num_docs=len(index[word])
+        IDF[word]=math.log((total_docs+1)/((num_docs+1)+1))
+        
+        for doc in index[word] :
+            scores[doc]+=index[word][doc]*w_count*IDF[word]
+        
+    scores=sorted(scores.items(), key=lambda x:x[1], reverse=True)
+    return scores
